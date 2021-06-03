19f23a30aee8b6666622681329b1583542258a52
nguyentrongty <62439357+nguyentrongty@users.noreply.github.com>
2021-06-03 15:45:48 +0700

    update

---
+++ employee.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE employee(
+    id int AUTO_INCREMENT PRIMARY KEY,
+    fullname varchar(255) not null,
+    dob varchar(255) not null,
+    address varchar(255) not null,
+    position varchar(255) not null,
+    department varchar(255) not null
+    );
+    INSERT INTO employee (fullname, dob, address, position, department) VALUES ('Nguyen Tri','01/01/1993','Ha Noi', 'Sinh Vien', 'FPTAPTECH');
+    
+    
+    
+    
+    
